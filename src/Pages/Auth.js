d402a7e03321231f766fcb6b20d2afdc8aad9bfd
castalonirenz <castalonirenz@gmail.com>
2023-01-12 21:54:45 +0800

    added: nodemon and login

--- src/Pages/Auth.js
+++ src/Pages/Auth.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import '../Utils/css/Auth.css'
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import axios from 'axios'
+import { url } from '../Utils/url';
+import { useNavigate } from 'react-router-dom';
 
 export const AuthPage = () => {
+
+    const navigate = useNavigate()
+    const [email, setEmail] = useState('renz.castaloni@tmcc.student.com')
+    const [password, setPassword] = useState('vKMK)Qhd2y^HN')
+
+
+    
+    const logMe = () => {
+
+        
+        axios.post(url +'login',null,{
+            params:{
+                email,
+                password
+            }
+        }).then(res => {
+            console.log(res, '--> check')
+
+            if(res.data.failed){
+                alert("Credentials not fouund, please try again.")
+            }else{
+                navigate('/student', {
+                    replace: true
+                })
+            }
+
+        }).catch(err => {
+            
+        })
+
+
+    }
 
     return(
 
@@ -15,26 +50,33 @@ export const AuthPage = () => {
             <div className='appname'>
                 Trece Martires National Highschool
             </div>
+            
 
 
-            <Form className='cardContainer'>
+            <div className='cardContainer'>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control value={email} type="email" placeholder="Enter email" onChange={(v) => {
+                        
+                        setEmail(v.target.value)
+                    }} />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Label >Password</Form.Label>
+                    <Form.Control value={password} onChange={(v) => {
+                        
+                        setPassword(v.target.value)
+                    }} type="password" placeholder="Password" />
                 </Form.Group>
         
-                <Button variant="primary" type="submit">
+                <Button onClick={() => logMe()} variant="primary" type="submit">
                     Submit
                 </Button>
-            </Form>
+            </div>
         </div>
     )
 
--- src/api/routes/login.js
+++ src/api/routes/login.js
@@ -8,33 +8,36 @@ const teacherModel = require('../model/teacherModel')
 router.post('/', function (req, res, next) {
 
 
-    let email = req.query.email
+    let schoolEmail = req.query.email
     let password = req.query.password
 
 
+    console.log(req)
     // Teacher
     // sample@tmcc.teacher.com
     // Admin
     // sample@tmcc.admin.com
 
-    let isTeacher = email.match(/tmcc.teacher.com/gi)
-    let isAdmin = email.match(/tmcc.admin.com/gi)
+    let isTeacher = schoolEmail.match(/tmcc.teacher.com/gi)
+    let isAdmin = schoolEmail.match(/tmcc.admin.com/gi)
     
     if(isTeacher != null){
         
-        teacherModel.find({ email, password }, "", (err, user) => {
+        teacherModel.find({ schoolEmail, password }, "", (err, user) => {
             if (err) return handleError(err);
             // 'athletes' contains the list of athletes that match the criteria.
             else {
                 if (user.length == 0) {
                     res.send({
-                        error: "Incorrect email or password, please try again."
+                        error: "Incorrect email or password, please try again.",
+                        failed: true
                     })
                 } else {
                     res.status(200)
                     res.send({
                         user,
-                        data: req.query
+                        data: req.query,
+                        failed: false
                     })
                 }
             }
@@ -49,19 +52,22 @@ router.post('/', function (req, res, next) {
     }
     else{
         // find all athletes who play tennis, selecting the 'name' and 'age' fields
-        studentModel.find({ email, password }, "", (err, user) => {
+        studentModel.find({ schoolEmail, password }, "", (err, user) => {
             if (err) return handleError(err);
             // 'athletes' contains the list of athletes that match the criteria.
             else {
                 if (user.length == 0) {
                     res.send({
-                        error: "Incorrect email or password, please try again."
+                        error: "Incorrect email or password, please try again.",
+                        student:true,
+                        failed: true
                     })
                 } else {
                     res.status(200)
                     res.send({
                         user,
-                        data: req.query
+                        data: req.query,
+                        failed: false
                     })
                 }
             }

ffd94b36868d823ff081c28af8ca8b6957e73f2b
castalonirenz <castalonirenz@gmail.com>
2023-02-11 01:30:10 +0800

    update

--- src/Pages/Auth.js
+++ src/Pages/Auth.js
@@ -14,8 +14,8 @@ export const AuthPage = () => {
 
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    const [email, setEmail] = useState('Renz.R@tmcc.teacher.com')
-    const [password, setPassword] = useState('t!)X3sLvAvcWK')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     const [showForgot, setForgot] = useState(false)
 
 
